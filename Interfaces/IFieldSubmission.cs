ab9d6d8fa4e8e0dbb6cbea45dd4518d8d6aad8b5
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:57:33 -0700

    Framework extraction update
    Framework extraction update

---
+++ Interfaces/IFieldSubmission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Forms.Abstractions.Interfaces
+{
+    public interface IFieldSubmission
+    {
+        string Name { get; }
+
+        string Value { get; }
+    }
+}
---
+++ Interfaces/IFormSubmission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Forms.Abstractions.Interfaces
+{
+    public interface IFormSubmission
+    {
+        IEnumerable<IFieldSubmission> Fields { get; }
+        string Name { get; }
+
+        Guid Owner { get; }
+    }
+}
