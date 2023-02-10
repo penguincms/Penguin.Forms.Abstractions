d7656124391aa1409c3be2ce31305c9778fec78b
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 13:30:16 -0700

    Autocommit
    Autocommit

--- Interfaces/IFormSubmission.cs
+++ Interfaces/IFormSubmission.cs
@@ -6,6 +6,7 @@ namespace Penguin.Forms.Abstractions.Interfaces
     public interface IFormSubmission
     {
         IEnumerable<IFieldSubmission> Fields { get; }
+
         string Name { get; }
 
         Guid Owner { get; }
